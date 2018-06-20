e834dbdd2ac6d94b24f7a9695a7359daa295e0d7
ohduran <alvaro.duranb@gmail.com>
2018-06-20 07:30:59 +0100

    Open AI Gym Set up

--- Deep Learning/TensorFlow Bootcamp/Reinforcement Learning/sample.py
+++ Deep Learning/TensorFlow Bootcamp/Reinforcement Learning/sample.py
@@ -1,1 +1,19 @@
+"""Let's explore how to create an example environment with Open AI Gym with CartPole.
+
+Goal of CartPole environment is to balance the pole on the cart. 0 moves to the left, 1 to the right.
+Environment is a numpy array with 4 floating point numbers: Horizontal position, Horizontal velocity, Angle of Pole, Angular velocity.
+"""
+
 import gym
+
+env = gym.make('CartPole-v0')
+
+env.reset () # default values
+
+for t in range(1000):
+
+    env.render()
+
+    env.step(env.action_space.sample()) # randomly choose an action from the available options
+
+    
