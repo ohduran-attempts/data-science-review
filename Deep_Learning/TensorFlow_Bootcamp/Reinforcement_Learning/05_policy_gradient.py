6e595c4a8c4524e77ac099b9aca2d4c468b95e0c
ohduran <alvaro.duranb@gmail.com>
2018-06-21 18:40:08 +0100

    Policy gradient - code along

--- Deep_Learning/TensorFlow_Bootcamp/Reinforcement_Learning/05_policy_gradient.py
+++ Deep_Learning/TensorFlow_Bootcamp/Reinforcement_Learning/05_policy_gradient.py
@@ -29,3 +29,68 @@ Steps:
 - Negative scores will create opposite gradients when multiplied.
 - Calculate mean of the resulting gradient vector for Gradient Descent.
 """
+
+import gym
+import tensorflow as tf
+import numpy as np
+
+num_inputs = 4
+num_hidden = 4
+num_outputs = 1
+
+learning_rate = 0.01
+
+initialize = tf.contrib.layers.variance_scaling_initializer()
+
+X = tf.placeholder(tf.float32, shape=[None, num_inputs])
+hidden_layer =tf.layers.dense(X, num_hidden, activation=tf.nn.relu, kernel_initializer=initializer)
+
+logit = tf.layers.dense(hidden_layer, num_outputs)
+outputs = tf.nn.sigmoid(logits)
+
+probabilities = tf.concat(axis=1, values = [outputs, 1 - outputs])
+actions = tf.multinomial(probabilities, num_samples=1)
+
+y = 1.0 - tf.to_float(action)
+
+# gradients off cross_entropy
+cross_entropy = tf.nn.sigmoid_cross_entropy_with_logits(labels=y, logits=logits)
+optimizer = tf.train.AdamOptimizer(learning_rate)
+
+gradients_and_variables = optimizer.compute_gradients(cross_entropy)
+
+
+# Compute gradients
+gradients = []
+gradient_placeholders = []
+grads_and_vars_feed = []
+
+for gradient, variable in gradients_and_variables:
+    gradients.append(gradient)
+    gradient_placeholder = tf.placeholder(tf.float32, shape=gradient.get_shape())
+    gradient_placeholders.append(gradient_placeholder)
+    grads_and_vars_feed.append(gradient_placeholder, variable)
+
+training_optimizer = optimizer.apply_gradients(grads_and_vars_feed)
+
+init =tf.global_variables_initializer()
+saver = tf.saver.Saver()
+
+def helper_discount_reward(rewards, discount_rate):
+    """"Takes in rewards and applies discount rate."""
+    discounted_rewards = np.zeros(len(rewards))
+    cumulative_rewards = 0
+    for step in reversed(range(len(rewards))):
+        cumulative_rewards = rewards[step] + cumulative_rewards*discount_rate
+        discounter_rewards[step] = cumulative_rewards
+    return discounted_rewards
+
+def discount_and_normalize_rewards(all_rewards, discount_rate):
+    """Takes in all rewards, applies helper_discount function and then normalizes using mean and std."""
+    all_discounted_rewards = [helper_discount_rewards(rewards, discount_rate) for rewards in all_rewards]
+
+    flat_rewards = np.concatenate(all_discounted_rewards)
+    reward_mean = flat_rewards.mean()
+    reward_std = flat_rewards.std()
+
+    return [(d_w - reward_mean) / reward_std for d_w in all_discounted_rewards]
